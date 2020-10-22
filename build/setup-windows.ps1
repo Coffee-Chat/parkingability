38a3ea99dc1b5a78d3b28741204c6bcd2b28f5d5
Daniel Stack <dstack@microsoft.com>
2020-10-22 17:32:24 -0500

    adding setup script for windows

---
+++ build/setup-windows.ps1
@@ -0,0 +1,47 @@
+# Require Admin
+
+function Test-IsRunningAsAdmin 
+{
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() )
+
+    if ($currentPrincipal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator ))
+    {
+        return $true
+    }
+    return $false
+}
+
+$IsAdmin = Test-IsRunningAsAdmin;
+if ($IsAdmin -eq $false) 
+{ 
+    throw "Setup requires PowerShell to be running as Administrator." 
+}
+
+# Install Choco
+
+&powershell -NoProfile -ExecutionPolicy unrestricted -Command `
+"&([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://chocolatey.org/install.ps1')))"
+
+# add choco to Path
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
+# Install Git for Windows
+choco install -y git
+
+# Install DotNetCli https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-install-script
+# Run a separate PowerShell process because the script calls exit, so it will end the current PowerShell session.
+
+&powershell -NoProfile -ExecutionPolicy unrestricted -Command `
+"&([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Architecture x64 -Channel Current -Runtime aspnetcore"
+
+# Install Azure CLI (or update) https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-windows?tabs=azure-cli
+
+&powershell -NoProfile -ExecutionPolicy unrestricted -Command `
+"Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi"
+
+# Install VSCode https://github.com/PowerShell/vscode-powershell
+
+&powershell -NoProfile -ExecutionPolicy unrestricted -Command `
+"&([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/PowerShell/vscode-powershell/master/scripts/Install-VSCode.ps1'))) -AdditionalExtensions ms-dotnettools.csharp,docsmsft.docs-authoring-pack,dbaeumer.vscode-eslint,eamodio.gitlens,ms-vscode.vscode-typescript-tslint-plugin,visualstudioexptteam.vscodeintellicode,redhat.vscode-yaml,ms-vsliveshare.vsliveshare-audio,ms-vsliveshare.vsliveshare,ms-vscode.powershell,ms-azure-devops.azure-pipelines,azurite.azurite"
+
+dotnet dev-certs https --trust
