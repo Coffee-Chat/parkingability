eeec5d6f52cc3974556cee84243402e6134af7d5
Daniel Stack <dstack@microsoft.com>
2020-10-23 13:15:34 -0500

    assume git, debug for ps, import to localmachine

--- build/setup-windows.ps1
+++ build/setup-windows.ps1
@@ -11,22 +11,32 @@ function Test-IsRunningAsAdmin
     return $false
 }
 
+function New-RandomPassword {
+    Param(
+        [ValidateRange(4,79)]
+        [int]    $Length = 16,
+        [switch] $ExcludeSpecialCharacters
+    )
+    $SpecialCharacters = @((33,35) + (36..38) + (42..44) + (60..64) + (91..94))
+    try {
+        if (-not $ExcludeSpecialCharacters) {
+                $Password = -join ((48..57) + (65..90) + (97..122) + $SpecialCharacters | Get-Random -Count $Length | foreach {[char]$_})
+            } else {
+                $Password = -join ((48..57) + (65..90) + (97..122) | Get-Random -Count $Length | foreach {[char]$_})  
+        }
+
+    } catch {
+        throw $_.Exception.Message
+    }
+
+    return $Password;
+}
+
 $IsAdmin = Test-IsRunningAsAdmin;
 if ($IsAdmin -eq $false) 
 { 
     throw "Setup requires PowerShell to be running as Administrator." 
 }
-
-# Install Choco
-
-&powershell -NoProfile -ExecutionPolicy unrestricted -Command `
-"&([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://chocolatey.org/install.ps1')))"
-
-# add choco to Path
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
-
-# Install Git for Windows
-choco install -y git
 
 # Install DotNetCli https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-install-script
 # Run a separate PowerShell process because the script calls exit, so it will end the current PowerShell session.
@@ -45,3 +55,10 @@ choco install -y git
 "&([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/PowerShell/vscode-powershell/master/scripts/Install-VSCode.ps1'))) -AdditionalExtensions ms-dotnettools.csharp,docsmsft.docs-authoring-pack,dbaeumer.vscode-eslint,eamodio.gitlens,ms-vscode.vscode-typescript-tslint-plugin,visualstudioexptteam.vscodeintellicode,redhat.vscode-yaml,ms-vsliveshare.vsliveshare-audio,ms-vsliveshare.vsliveshare,ms-vscode.powershell,ms-azure-devops.azure-pipelines,azurite.azurite"
 
 dotnet dev-certs https --trust
+
+# import certificate to localmachine store
+$NewPassword = New-RandomPassword -ExcludeSpecialCharacters
+$PfxFilePath = ".\localhost.pfx"
+dotnet dev-certs https -ep $PfxFilePath -p $NewPassword
+$PasswordSecure = ConvertTo-SecureString -String $NewPassword -Force -AsPlainText
+Import-PfxCertificate -Exportable -FilePath $PfxFilePath -CertStoreLocation 'Cert:\LocalMachine\My' -Password $PasswordSecure -Verbose
